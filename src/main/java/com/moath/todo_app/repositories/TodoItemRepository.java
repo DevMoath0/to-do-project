8806d223c68d472ff4c632bc18bc7053e8947963
Moath Khalid Alkhelifi <mkalkhelifi@barq.com>
2024-12-29 16:31:44 +0300

    Todo Project Finished

---
+++ src/main/java/com/moath/todo_app/repositories/TodoItemRepository.java
@@ -0,0 +1,9 @@
+package com.moath.todo_app.repositories;
+
+import com.moath.todo_app.models.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+}
